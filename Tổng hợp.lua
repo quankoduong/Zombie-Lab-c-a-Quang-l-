bad4f45dcb29bfc1772f917603fc592429978783
Quang lỏ <trangiang0948620.558@gmail.com>
2025-09-07 16:50:33 +0700

    Add files via upload

---
+++ Tổng hợp.lua
@@ -0,0 +1,45 @@
+ loadstring(game:HttpGet(("https://raw.githubusercontent.com/daucobonhi/Ui-Redz-V2/refs/heads/main/UiREDzV2.lua")))()
+
+       local Window = MakeWindow({
+         Hub = {
+         Title = "Quang lỏ 7B",
+         Animation = "Châu Chấu 7B :))"
+         },
+        Key = {
+        KeySystem = false,
+        Title = "Key System",
+        Description = "Key: (7B)  :))",
+        KeyLink = "https://link4sub.com/note/H56L",
+        Keys = {"7B"},
+        Notifi = {
+        Notifications = true,
+        CorrectKey = "Chạy Script...",
+       Incorrectkey = "key không đúng",
+       CopyKeyLink = "đã copy link key"
+      }
+    }
+  })
+
+       MinimizeButton({
+       Image = "http://www.roblox.com/asset/?id=75928273719337",
+       Size = {60, 60},
+       Color = Color3.fromRGB(10, 10, 10),
+       Corner = true,
+       Stroke = false,
+       StrokeColor = Color3.fromRGB(255, 0, 0)
+      })
+      
+------ Tab
+     local Tab1o = MakeTab({Name = "🍍blox fruit🍍"})
+     local Tab2o = MakeTab({Name = "🫛Grow a Garden🫛"})
+
+------- BUTTON
+    
+    AddButton(Tab1o, {
+     Name = "blue x",
+    Callback = function()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Dev-BlueX/BlueX-Hub/refs/heads/main/Main.lua"))()
+
+AddButton(Tab2o, {
+     Name = "🌑Coming Soon🌑",
+    Callback = function()
